761f3b7a9e7a360d344b83fc43d2d514b6b8cdf2
Rachel Heck <heck.rachel4@gmail.com>
2022-07-15 13:28:54 -0400

    T-test on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,13 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_data <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- suspension_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), StDev=sd(PSI),.groups = 'keep') 
 lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), StDev=sd(PSI),.groups = 'keep')
+
+# T-Tests on Suspension Coils
+?t.test()
+t.test(x=suspension_data$PSI, alternative = "two.sided", mu = 1500)
+subs_1 <- which(suspension_data$Manufacturing_Lot=="Lot1")
+subs_2 <- which(suspension_data$Manufacturing_Lot=="Lot2")
+subs_3 <- which(suspension_data$Manufacturing_Lot=="Lot3")
+t.test(x=suspension_data$PSI, alternative = "two.sided", mu = 1500, subset = subs_1)
+t.test(x=suspension_data$PSI, alternative = "two.sided", mu = 1500, subset = subs_2)
+t.test(x=suspension_data$PSI, alternative = "two.sided", mu = 1500, subset = subs_3)
